c3e881fcd14547e02018a8bf8f47e40d66a7e059
Chelsea Isaacson <chelseaisaacson@Chelseas-MacBook-Air.local>
2020-04-14 16:38:02 -0700

    MVP day 2

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -9,7 +9,7 @@ def selection_sort( arr ):
         for j in range(cur_index + 1, len(arr)):
             if arr[smallest_index] > arr[j]:
                 smallest_index = j
-        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
+        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
 
              
      # TO-DO: swap
@@ -19,25 +19,33 @@ def selection_sort( arr ):
 
     return arr
 
+arr = [1,5,6,3,4,8]
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
     for i in range(len(arr)):
-        print('value i', arr[i])
-        # # print('index i', i)
+        # print('value i', arr[i])
+        # print('index i', i)
         for j in range(0, len(arr)-i-1):
-            print('value j', arr[j])
-        #     # print('index j', j)
+            # print('value j', arr[j])
+            # print('index j', j)
             if arr[j] > arr[j+1]:
-                print(arr[j], arr[j+1])
+                # print(arr[j], arr[j+1])
                 arr[j], arr[j+1] = arr[j+1], arr[j]
-                print(arr)
+                # print(arr)
     return arr
 
-arr = [1,5,6,3,4,8]
-print(bubble_sort(arr))
+# def bubble_sort(arr):
+#     swaps_occured = True
+#     while swaps_occured:
+#         swaps_occured = False
+    
+#         for i in range(len(arr)-1):
+#             if arr[i] > arr[i+1]:
+#                 arr[i], arr[i+1] = arr[i+1], arr[i]
+#                 swaps_occured = True
 
-
+    # return arr
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
 
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,15 +3,42 @@ def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
-    
+
+    aIndex = 0
+    bIndex = 0
+    for i in range(len(merged_arr)):
+        if aIndex == len(arrA) and bIndex <= len(arrB):
+            merged_arr[i] = arrB[bIndex]            
+            bIndex += 1
+        elif aIndex <= len(arrA) and bIndex == len(arrB):
+            merged_arr[i] = arrA[aIndex]            
+            aIndex += 1
+        elif arrA[aIndex] < arrB[bIndex]:
+            merged_arr[i] = arrA[aIndex]
+            aIndex += 1
+        elif arrA[aIndex]> arrB[bIndex]:
+            merged_arr[i]= arrB[bIndex]
+            bIndex += 1
+
     return merged_arr
 
+
+a = [1,3,4]
+b = [2,5,7]
+print(merge(a, b))
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    if len(arr) > 1:
+        left = merge_sort(arr[:len(arr)// 2])
+        right = merge_sort(arr[len(arr)// 2:])
+        arr = merge(left, right)
 
     return arr
+
+hello = [5,3,7,2,8,9,1,3,0]
+print(merge_sort(hello))
 
 
 # STRETCH: implement an in-place merge sort algorithm
